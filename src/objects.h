26cd9a55bddedf593a4fc4298f16b7a0d67c2e1e
catalin iulian <ancutei.catalin@gmail.com>
2023-08-28 00:00:20 +0300

    added javadoc comments

--- src/objects.h
+++ src/objects.h
@@ -15,18 +15,42 @@ enum sylk_object_type {
     SYLK_OBJ_COUNT    = 7
 };
 
+
+/**
+ * functions and classes can have two types of implementation,
+ * either builtin_in in interpreter or in code
+ */
 enum sylk_implementation_type {
     SYLK_USER     = 0,
     SYLK_BUILT_IN = 1
 };
 
+
+/**
+ * function type for deleting an user object
+ *
+ * @param mem pointer to memory
+ */
 typedef void (*sylk_free_function)(void* mem);
 
+
+/**
+ * @field mem memory pointer
+ * @field free_fun callback function to free the object
+ */
 struct sylk_object_user {
     void* mem;
     sylk_free_function free_fun;
 };
 
+
+/**
+ * @field type object type, should be a value of sylk_object_type
+ * @field num_value object value if object is number
+ * @field str_value object value if object is string
+ * @field str_value object value if object is string
+ * @field obj_value object value if object is an head object
+ */
 struct sylk_object {
     int32_t type;
 
@@ -38,9 +62,31 @@ struct sylk_object {
     };
 };
 
+
 struct sylk_vm;
+
+
+/**
+ * function definition for an user defined function
+ *
+ * @param self instance reference
+ * @param vm sylk virtual machine instance
+ * @param ctx user provided context in "sylk_new"
+ *
+ * @return function return value to pe pushed on the stack
+ */
 typedef struct sylk_object (*builtin_fun)(struct sylk_object* self, struct sylk_vm* vm, void* ctx);
 
+
+/**
+ * structure representing a function object
+ *
+ * @field type function type, can be SYLK_BUILTIN or SYLK_USER
+ * @field index index in the bytecodes of the function if function is SYLK_USER
+ * @field function the function callback if the function is SYLK_BUILT_IN
+ * @field n_parameters function parameters number
+ * @field context in case the function is a method, context store the instance
+ */
 struct sylk_object_function {
     int32_t type;
 
@@ -53,19 +99,55 @@ struct sylk_object_function {
     struct sylk_object context;
 };
 
+
+/**
+ * @field name function name
+ * @field function function object
+ */
 struct sylk_named_function {
     const char* name;
     struct sylk_object_function function;
 };
 
+
+/**
+ * structure representing an instance object
+ *
+ * @field cls class object
+ * @field members instance members
+ */
 struct sylk_object_instance {
     struct sylk_object_class* cls;
     struct sylk_object members[256];
 };
 
+
 typedef void (*sylk_object_callback)(struct sylk_object* o, void* ctx);
+
+
+/**
+ * callback provided by used to iterate through all of the referred objects
+ * of the user implemented class
+ *
+ * @param self instance reference
+ * @param cb callback to pe called with every object
+ * @param ctx context to be passed to callback
+*/
 typedef void (*sylk_iterate_function)(struct sylk_object_instance* self, sylk_object_callback cb, void* ctx);
 
+
+/**
+ * structure representing a class object
+ *
+ * @field type class type, can be one of SYLK_USER or SYLK_BUILT_IN
+ * @field index bytecode index if the class is SYLK_USER
+ * @field iterate_fun function callback to iterate through all of the references
+ * @field constructor index of the method used as a constructor
+ * @field members names of the members of the class
+ * @field n_members count
+ * @field class methods
+ * @field methods count
+ */
 struct sylk_object_class {
     int32_t type;
 
@@ -82,6 +164,11 @@ struct sylk_object_class {
     uint32_t n_methods;
 };
 
+
+/**
+ * @field name class name
+ * @field cls class object
+ */
 struct sylk_named_class {
     const char* name;
     struct sylk_object_class cls;
--- src/sylk.h
+++ src/sylk.h
@@ -5,17 +5,20 @@
 #include <stdbool.h>
 #include "objects.h"
 
+
 struct sylk_config {
     bool print_ast;
     bool print_bytecode;
     bool halt_program;
 };
 
+
 struct sylk_function {
     const char* name;
     builtin_fun function;
     size_t n_parameters;
 };
+
 
 struct sylk_class {
     const char* name;
@@ -24,20 +27,120 @@ struct sylk_class {
     struct sylk_function* methods;
 };
 
+
 struct sylk;
+
+
+/**
+ * create a new interpreter instance
+ *
+ * @param config interpreter configuration
+ * @param ctx user provided context propagated to all functions
+ *
+ * @return interpreter instance
+ */
 struct sylk* sylk_new(const struct sylk_config* config, void* ctx);
+
+
+/**
+* free interpreter resources
+*
+* @param s sylk instance
+*/
 void sylk_free(struct sylk* s);
 
+
+/**
+ * load all of the default functions and classes like "list" and "print"
+ *
+ * @param s sylk instance
+ *
+ * @return success code
+ */
 int sylk_load_prelude(struct sylk* s);
+
+
+/**
+ * load user defined functions to sylk interpreter
+ *
+ * @param s interpreter instance
+ * @param functions user functions to load in prelude (last element should contain NULL and 0)
+ *
+ * @return success code
+ */
 int sylk_load_functions(struct sylk* s, struct sylk_function* functions);
+
+
+/**
+ * load user defined classes to sylk interpreter
+ *
+ * @param s interpreter instance
+ * @param classes user classes to load in prelude (last element should contain NULL and 0)
+ *
+ * @return success code
+ */
 int sylk_load_classes(struct sylk* s, struct sylk_class* classes);
 
+
+/**
+ * run program stored in string
+ *
+ * @param s interpreter instance
+ * @param program program code to run
+ * @param program_size length of program
+ *
+ * @return success code
+ */
 int sylk_run_string(struct sylk* s, const char* program, size_t program_size);
+
+
+/**
+ * run program from file
+ *
+ * @param s interpreter instance
+ * @file_name file to run
+ *
+ * @return success code
+ */
 int sylk_run_file(struct sylk* s, const char* file_name);
 
+
+/**
+ * virtual machine instance
+ */
 struct sylk_vm;
+
+
+/**
+ * push object on the stack
+ *
+ * @param vm virtual machine instance
+ * @param o object to push
+ *
+ * @return success code
+ */
 int sylk_push(struct sylk_vm* vm, const struct sylk_object* o);
+
+
+/**
+ * pop an object from the top of the stack
+ *
+ * @param vm virtual machine instance
+ * @param o where the object will be stored
+ *
+ * @return success code
+ */
 int sylk_pop(struct sylk_vm* vm, struct sylk_object* o);
+
+
+/**
+ * peek an object from the top of the stack without popping
+ *
+ * @param vm virtual machine instance
+ * @param o where the object will be stored
+ *
+ * @return success code
+ */
 int sylk_peek(struct sylk_vm* vm, size_t n, struct sylk_object* o);
 
 #endif
